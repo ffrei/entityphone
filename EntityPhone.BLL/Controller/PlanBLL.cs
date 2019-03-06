07c9f56d6fee6005e5c62db569f8fd6be6a9d7d3
Anthony <anthony.kirmann@actimage.com>
2019-03-06 16:02:37 +0100

    Add Plan BLL and DM

---
+++ EntityPhone.BLL/Controller/PlanBLL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityPhone.DM.Controller;
+using EntityPhone.Model;
+
+namespace EntityPhone.BLL.Controller
+{
+    public class PlanBLL
+    {
+        PlanDAL planDAL;
+        public PlanBLL()
+        {
+            planDAL = new PlanDAL();
+        }
+        public IPlan Create(string name, int minute_limit, int sms_limit, decimal price, decimal overage_minute_price, decimal overage_sms_price, bool is_available)
+        {
+            return planDAL.Create(name, minute_limit, sms_limit, price, overage_minute_price, overage_sms_price, is_available);
+        }
+
+        public List<IPlan> GetAll()
+        {
+            return planDAL.GetAll();
+        }
+
+        public IPlan GetById(int id)
+        {
+            return planDAL.GetById(id);
+        }
+
+        public void Update(IPlan plan)
+        {
+            planDAL.Update(plan);
+        }
+        public void Delete(IPlan plan)
+        {
+            planDAL.Delete(plan);
+        }
+
+    }
+}
---
+++ EntityPhone.DM/Controller/PlanController.cs
@@ -0,0 +1,75 @@
+﻿using EntityPhone.DM.Model;
+using EntityPhone.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityPhone.DM.Controller
+{
+    public class PlanDAL
+    {
+        public IPlan Create(string name, int minute_limit, int sms_limit, decimal price, decimal overage_minute_price, decimal overage_sms_price, bool is_available)
+        {
+            using (var context = new EPEntities())
+            {
+                plan c = new plan()
+                {
+                    name = name,
+                    minute_limit = minute_limit,
+                    sms_limit = sms_limit,
+                    price = price,
+                    overage_minute_price = overage_minute_price,
+                    overage_sms_price = overage_sms_price,
+                    is_available = is_available
+                };
+                context.plan.Add(c);
+                context.SaveChanges();
+                if (c.plan_id > 0)
+                {
+                    return c;
+                }
+                return null;
+            }
+        }
+
+        public List<IPlan> GetAll()
+        {
+            using (var context = new EPEntities())
+            {
+                List<IPlan> res = new List<IPlan>();
+                foreach (IPlan plan in context.plan.ToList())
+                {
+                    res.Add(plan);
+                }
+                return res;
+            }
+        }
+        public IPlan GetById(int id)
+        {
+            using (var context = new EPEntities())
+            {
+                return context.plan.Find(id);
+            }
+        }
+
+        public void Update(IPlan plan)
+        {
+            using (var context= new EPEntities())
+            {
+                context.Entry(plan).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void Delete (IPlan plan)
+        {
+            using (var context = new EPEntities())
+            {
+                context.Entry(plan).State = EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+    }
+}
